7c1c956d3bca001d3e5b0bc6accb0fb512d08798
srenkens <srenkens@gmail.com>
2015-09-25 15:30:10 +0200

    Corrected URL for use in protracor container

--- src/main/test/e2e/homepage-spec.js
+++ src/main/test/e2e/homepage-spec.js
@@ -1,7 +1,7 @@
 describe('SportsQuest Demo App', function() {
 
 	it('should have a title', function() {
-		browser.get('http://localhost:8888/sportsquest-web-1.0-b1');
+		browser.get('http://sportsquest:8080/sportsquest-web-1.0-b1');
 		var caroulsel = browser.findElement(by.css('.carousel'));
 		expect(browser.getTitle()).toEqual('Sportsquest app');
 	});
