302bdd5958cecc59ebbda1eb148f6c71fdfdd8c5
Johan Vogelzang <johan.vogelzang@gmail.com>
2015-09-25 14:43:24 +0200

    Moved tests config to one folder

--- src/main/test/e2e/homepage-spec.js
+++ src/main/test/e2e/homepage-spec.js
@@ -3,7 +3,6 @@ describe('SportsQuest Demo App', function() {
 	it('should have a title', function() {
 		browser.get('http://localhost:8888/sportsquest-web-1.0-b1');
 		var caroulsel = browser.findElement(by.css('.carousel'));
-//		browser.actions().mouseMove(carousel).find();
 		expect(browser.getTitle()).toEqual('Sportsquest app');
 	});
 });
